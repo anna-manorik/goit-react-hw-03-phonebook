7f2f6d297fd2efdee8c23a39aecf2bfb2a72cb7b
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-02 21:30:50 +0200

    Update NameItem.js

--- src/components/NameItem/NameItem.js
+++ src/components/NameItem/NameItem.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import styles from './nameItem.module.css';
 
 const NameItem = ({ visibleContacts, onDeleteContact }) => (
-    
-    <>
-        {visibleContacts.map(({ id, name, number }) => 
-        <li key={id} className={styles.item}> {name}: {number} <button className={styles.button} onClick={() => onDeleteContact(id)}>Delete contact</button></li>)}
-        
-    </>
+  <>
+    {visibleContacts.map(({ id, name, number }) => (
+      <li key={id} className={styles.item}>
+        {name}: {number}
+        <button className={styles.button} onClick={() => onDeleteContact(id)}>
+          Delete contact
+        </button>
+      </li>
+    ))}
+  </>
 );
 
 NameItem.propTypes = {
-    visibleContacts: PropTypes.array,
-    onDeleteContact: PropTypes.func,
-}
+  visibleContacts: PropTypes.array,
+  onDeleteContact: PropTypes.func,
+};
 
 export default NameItem;
