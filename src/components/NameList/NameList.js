20c52acdd5c8d4eacff038b7c7d8639b9a7eeea7
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-02 21:30:52 +0200

    Update NameList.js

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import styles from './nameList.module.css';
 
-const NameList = ({ children }) => (
-    <ul className={styles.list}>
-        {children}
-    </ul>
-)
+const NameList = ({ children }) => <ul className={styles.list}>{children}</ul>;
 
 NameList.propTypes = {
-    children: PropTypes.element,
-}
+  children: PropTypes.element,
+};
 
-export default NameList; 
+export default NameList;
