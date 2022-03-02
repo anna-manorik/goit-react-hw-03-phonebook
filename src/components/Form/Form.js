e3c29d2886922061b6dcde7405eefd50bbae7245
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-02 21:30:46 +0200

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,35 +1,43 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import styles from './form.module.css';
 
 const Form = ({ handleSubmit }) => (
-    <form onSubmit={handleSubmit}>
-    <label> Name: 
-        <input
-            className={styles.input}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-        />
-    </label><br />
-    <label> Phone num: 
-        <input
-            className={styles.input}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-        />
-    </label><br />
-    <button type='submit' className={styles.button}>Add contact</button>
-    </form>
+  <form onSubmit={handleSubmit}>
+    <label>
+      {' '}
+      Name:
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+    </label>
+    <br />
+    <label>
+      {' '}
+      Phone num:
+      <input
+        className={styles.input}
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+    </label>
+    <br />
+    <button type="submit" className={styles.button}>
+      Add contact
+    </button>
+  </form>
 );
 
 Form.propTypes = {
-    handleSubmit: PropTypes.func,
-}
+  handleSubmit: PropTypes.func,
+};
 
 export default Form;
