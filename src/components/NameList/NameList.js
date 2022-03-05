bd541bdd27accf30bdd2b3871195cd2a973e8cc7
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-03-05 14:08:39 +0200

    Update NameList.js

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import NameItem from '../NameItem/NameItem';
 import styles from './nameList.module.css';
 
-const NameList = ({ children }) => <ul className={styles.list}>{children}</ul>;
+const NameList = ({ visibleContacts, onDeleteContact }) => (
+  <ul className={styles.list}>
+    {visibleContacts.map(({ id, name, number }) => (
+      <NameItem
+        id={id}
+        name={name}
+        number={number}
+        onDeleteContact={onDeleteContact}
+      />
+    ))}
+  </ul>
+);
 
 NameList.propTypes = {
-  children: PropTypes.element,
+  visibleContacts: PropTypes.array,
+  onDeleteContact: PropTypes.func,
 };
 
 export default NameList;
